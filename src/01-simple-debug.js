f69a4272e03e9b7a7fb0a3e40779cc8ded05cde9
Demetrio <dlima.nyit@gmail.com>
2023-08-24 11:24:56 -0700

    Finishing Object ball

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,40 @@
-console.log('Simple debugging example running.')
-debugger
+function homeTeamName(){
+    let game = gameObject()
+    return game.home.teamName
+}
 
-let x = 99
-debugger
-console.log(x)
+
+homeTeamName()
+
+/**
+ * for of
+ *  [1, "1", 1 + 1, true , false]
+*    1  1   2  true false
+ * for in
+ *   [1, "1", 1 + 1, true , false]
+*    0  1   2      3          4
+
+ * for let i = 0, array.length > i; i++
+ *   
+ * 
+ * while(i > 0){
+ *  do whatever functionality
+ * 
+ *  false
+ * }
+ * 
+ * forEach (element, index, array)
+ *  Perform the code that you're executing
+ * 
+ * map (element, index, array)
+ *  return a new array depending on the code you're executing
+ * 
+ * filter (element, index, array) assign to a variable => []
+ *  Will return an array of elements that meet true for criteria that you set 
+ *  ex. name === "Demetrio"
+ * 
+ * find (element, index, array) assign to a variable => undefined
+ *  Will return an the first element that meet true for criteria that you set
+ *  
+ */
+
